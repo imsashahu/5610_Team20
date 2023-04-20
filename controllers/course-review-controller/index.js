37bee4e48f94a3f4ef25aed59d7c7d8a6a4dae46
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-19 17:24:27 -0700

    Added end point for getting all course numbers

--- controllers/course-review-controller/index.js
+++ controllers/course-review-controller/index.js
@@ -114,6 +114,24 @@ const getAllReviewsByUserId = async (req, res, next) => {
   }
 };
 
+// Get all course numbers.
+const getAllCourseNumbers = async (req, res, next) => {
+  try {
+    const courses = await courseDao.getAll();
+    console.log("[getAllCourseNumbers] coursees", courses);
+    const courseNumbers = [];
+    courses.forEach((course) => {
+      courseNumbers.push(course.courseNumber);
+    });
+    res.json(courseNumbers);
+
+    // const courseNumbers = await courseDao.getAllCourseNumbers();
+    // res.json(courseNumbers);
+  } catch (err) {
+    next(err);
+  }
+};
+
 export default (app) => {
   // courses
   app.get("/courses", getAllCourses);
@@ -128,4 +146,5 @@ export default (app) => {
     deleteReviewByIdForCourse
   );
   app.get("/reviews/:userId", getAllReviewsByUserId);
+  app.get("/all-course-numbers", getAllCourseNumbers);
 };
