2d2f10757f01d2f3f1b24da58433c3e22ee4626c
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-23 03:10:54 -0700

    Set cors to defualt policy

--- app.js
+++ app.js
@@ -18,15 +18,7 @@ app.use(
 );
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "https://startling-marigold-eab23a.netlify.app/",
-    ],
-  })
-);
+app.use(cors());
 
 const CONNECTION_STRING = `${process.env.CONNECTION_STRING}`;
 mongoose.connect(CONNECTION_STRING);
